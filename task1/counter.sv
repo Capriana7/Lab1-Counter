ca43317872309abff2bdca022ff6e675d400fb1b
Ananya Gupta <ananyag987@example.com>
2025-10-25 12:50:52 +0100

    implemented task 1 "test yourself challenge"

--- task1/counter.sv
+++ task1/counter.sv
@@ -7,7 +7,7 @@ module counter #(
     output logic [WIDTH-1:0] count
 );
 
-always_ff @ (posedge clk)
+always_ff @ (posedge clk, posedge rst) // asynchronous reset added by adding rst to sensitivity list
     if (rst) count <= {WIDTH{1'b0}};
     else count <= count + {{WIDTH-1{1'b0}}, en};
 
--- task1/counter_tb.cpp
+++ task1/counter_tb.cpp
@@ -18,6 +18,9 @@ int main(int argc, char **argv, char **env){
     top->clk = 1;
     top->rst = 1;
     top->en = 0;
+    int count = top->count;
+    bool pause = false;
+    int pause_cycles = 0;
 
     for(i=0; i<300;i++){
         for(clk=0; clk<2; clk++){
@@ -25,8 +28,25 @@ int main(int argc, char **argv, char **env){
             top->clk = !top->clk;
             top->eval ();
         }
-        top->rst = (i<2) | (i==15);
-        top->en = (i<4);
+        top->rst = (i<2) | (i==20) | (i==50); // random resets added
+        if (top->clk) {
+            int count = top->count;
+            if (!pause && count == 9) { // pause implemented on first cycle of count hitting 9
+                pause = true;
+                pause_cycles = 0;
+            }
+            if (pause) {
+                top->en = 0;
+                pause_cycles++;
+                if (pause_cycles > 3){ // waits for 3 whole clock cycles of enable being low
+                    pause = false;
+                    top->en = 1;
+                }
+            } 
+            else if (i<100){ // if it is not paused or 9, it will keep counting until 100 clock cycles
+                top->en = 1;
+            }
+        }
         if (Verilated::gotFinish()) exit(0);
     }
     tfp->close();
